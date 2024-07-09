ae4d7ae437ad26993047824b1c40d2124391169f
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-07-09 22:25:27 +0530

    Toggling functionality in admin panel box

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./Logo";
 import { GrSearch } from "react-icons/gr";
 import { FaRegCircleUser } from "react-icons/fa6";
@@ -13,6 +13,7 @@ import { setUserDetails } from '../store/userSlice';
 const Header = () => {
   const user = useSelector(state => state?.user?.user);
   const dispatch = useDispatch();
+  const [menuDisplay, setMenuDisplay] = useState(false);
 
   console.log("user header", user);
 
@@ -55,14 +56,28 @@ const Header = () => {
         </div>
 
         <div className="flex items-center gap-7">
-          <div className="text-3xl cursor-pointer">
+          <div className="relative flex justify-center">
+          <div className="text-3xl cursor-pointer relative flex justify-center" onClick={()=>setMenuDisplay((prev) => !prev)}>
             {
               user?.profilePic ? (
               <img src={user?.profilePic} alt={user?.name} className="w-10 h-10 rounded-full"/>
             ) : (<FaRegCircleUser />)
             }
-            
           </div>
+
+            {
+              menuDisplay && (
+                <div className="absolute bg-white bottom-0 top-11 h-fit p-2 shadow-lg rounded">
+                <nav>
+                  <Link to={"admin-panel"} className="whitespace-nowrap hover:bg-slate-100 p-2">Admin Panl</Link>
+                </nav>
+            </div>
+              )
+            }
+
+
+          </div>
+
 
           <div className="text-2xl relative">
             <span>
