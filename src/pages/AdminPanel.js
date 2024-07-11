91f6b773059c25914f32ff4280f0ae15fca1a6d7
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-07-11 12:31:56 +0530

    set restriction if the login user is not an admin

--- src/pages/AdminPanel.js
+++ src/pages/AdminPanel.js
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { useSelector } from "react-redux";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import ROLE from "../common/role";
 
 const AdminPanel = () => {
   const user = useSelector((state) => state?.user?.user);
-  const [menuDisplay, setMenuDisplay] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(()=> {
+    if(user?.role !== ROLE.ADMIN) {
+      navigate("/");
+    }
+  }, [user])
+  
+
   return (
     <div className="min-h-[calc(100vh-120px)] md:flex hidden">
       <aside className="bg-white min-h-full w-full max-w-60 customShadow">
