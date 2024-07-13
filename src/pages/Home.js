c2278c99be0eb2ca3bbb192454032c68117f3964
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-07-13 20:19:18 +0530

    fetch categorywise product and display in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import CategoryList from '../components/CategoryList'
 import BannerProduct from '../components/BannerProduct'
+import HorizontalCardProduct from '../components/HorizontalCardProduct'
 
 const Home = () => {
   return (
     <div>
       <CategoryList />
       <BannerProduct />
+
+      <HorizontalCardProduct category={"airpodes"} heading={"Top's Airpodes"}/>
     </div>
   )
 }
