54dfb66cb0395ffe8cdfd132b8729cd6305e2428
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-07-15 22:59:52 +0530

    product details component for Loading state

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -100,36 +100,66 @@ const ProductDetails = () => {
             </div>
         </div>
         {/* Product details */}
-        <div className='flex flex-col gap-1'>
+        {
+          loading ? (
+            <div className='grid gap-1 w-full'>
+            <p className='bg-slate-200 animate-pulse  h-6 lg:h-8 w-full rounded-full inline-block'></p>
+            <h2 className='text-2xl lg:text-4xl font-medium h-6 lg:h-8  bg-slate-200 animate-pulse w-full'></h2>
+            <p className='capitalize text-slate-400 bg-slate-200 min-w-[100px] animate-pulse h-6 lg:h-8  w-full'></p>
 
-          <p className='bg-red-200 text-red-600 px-2 rounded-full inline-block w-fit'>{data?.brandName}</p>
-          <h2 className='text-2xl lg:text-4xl font-medium'>{data?.productName}</h2>
-          <p className='capitalize text-slate-400'>{data?.category}</p>
+            <div className='text-red-600 bg-slate-200 h-6 lg:h-8  animate-pulse flex items-center gap-1 w-full'>
 
-          <div className='text-red-600 flex items-center gap-1'>
-            <FaStar/>
-            <FaStar/>
-            <FaStar/>
-            <FaStar/>
-            <FaStarHalf />
+            </div>
+
+            <div className='flex items-center gap-2 text-2xl lg:text-3xl font-medium my-1 h-6 lg:h-8  animate-pulse w-full'>
+              <p className='text-red-600 bg-slate-200 w-full'></p>
+              <p className='text-slate-400 line-through bg-slate-200 w-full'></p>
+            </div>
+
+            <div className='flex items-center gap-3 my-2 w-full'>
+              <button className='h-6 lg:h-8  bg-slate-200 rounded animate-pulse w-full'></button>
+              <button className='h-6 lg:h-8  bg-slate-200 rounded animate-pulse w-full'></button>
+            </div>
+
+            <div className='w-full'>
+              <p className='text-slate-600 font-medium my-1 h-6 lg:h-8   bg-slate-200 rounded animate-pulse w-full'></p>
+              <p className=' bg-slate-200 rounded animate-pulse h-10 lg:h-12  w-full'></p>
+            </div>
           </div>
+          ) : (
+            <div className='flex flex-col gap-1'>
 
-          <div className='flex items-center gap-2 text-2xl lg:text-2xl font-medium my-1'>
-            <p className='text-red-600'>{displayINRCurrency(data.sellingPrice)}</p>
-            <p className='text-slate-400 line-through'>{displayINRCurrency(data.price)}</p>
-          </div>
+            <p className='bg-red-200 text-red-600 px-2 rounded-full inline-block w-fit'>{data?.brandName}</p>
+            <h2 className='text-2xl lg:text-4xl font-medium'>{data?.productName}</h2>
+            <p className='capitalize text-slate-400'>{data?.category}</p>
+  
+            <div className='text-red-600 flex items-center gap-1'>
+              <FaStar/>
+              <FaStar/>
+              <FaStar/>
+              <FaStar/>
+              <FaStarHalf />
+            </div>
+  
+            <div className='flex items-center gap-2 text-2xl lg:text-2xl font-medium my-1'>
+              <p className='text-red-600'>{displayINRCurrency(data.sellingPrice)}</p>
+              <p className='text-slate-400 line-through'>{displayINRCurrency(data.price)}</p>
+            </div>
+  
+            <div className='flex items-center gap-3 my-2'>
+                <button className='border-2 border-red-600 rounded px-3 py-1 min-w-[120px] text-red-600 font-medium hover:bg-red-600 hover:text-white'>Buy</button>
+                <button className='border-2 border-red-600 rounded px-3 py-1 min-w-[120px] font-medium text-white bg-red-600 hover:text-red-600 hover:bg-white'>Add To Cart</button>
+            </div>
+  
+            <div>
+              <p className='text-slate-600 font-medium my-1'>Description : </p>
+              <p>{data?.description}</p>
+            </div>
+  
+          </div> 
+          )
+        }
 
-          <div className='flex items-center gap-3 my-2'>
-              <button className='border-2 border-red-600 rounded px-3 py-1 min-w-[120px] text-red-600 font-medium hover:bg-red-600 hover:text-white'>Buy</button>
-              <button className='border-2 border-red-600 rounded px-3 py-1 min-w-[120px] font-medium text-white bg-red-600 hover:text-red-600 hover:bg-white'>Add To Cart</button>
-          </div>
-
-          <div>
-            <p className='text-slate-600 font-medium my-1'>Description : </p>
-            <p>{data?.description}</p>
-          </div>
-
-        </div>
       </div>
 
     </div>
