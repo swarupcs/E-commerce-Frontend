74eec0e11a0e126d906c0ed46267f570c89d406d
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-07-16 19:16:03 +0530

    some fix

--- src/components/VerticalCardProduct.js
+++ src/components/VerticalCardProduct.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import fetchCategoryWiseProduct from '../helpers/fetchCategoryWiseProduct';
 import displayINRCurrency from './../helpers/displayCurrency';
 import { FaAngleRight } from "react-icons/fa6";
 import { FaAngleLeft } from "react-icons/fa6";
 import addToCart from '../helpers/addToCart';
 import { Link } from 'react-router-dom';
+import Context from '../context';
 
 const VerticalCardProduct = ({category,heading}) => {
 
@@ -14,6 +15,14 @@ const VerticalCardProduct = ({category,heading}) => {
 
     const [scroll, setScroll] = useState(0);
     const scrollElement = useRef();
+
+    
+    const { fetchUserAddToCart } = useContext(Context)
+
+    const handleAddToCart = async(e,id)=>{
+       await addToCart(e,id)
+       fetchUserAddToCart()
+    }
 
     const fetchData = async() => {
         setLoading(true);
@@ -80,7 +89,7 @@ const VerticalCardProduct = ({category,heading}) => {
                                 <p className='text-slate-500 line-through'>{displayINRCurrency(product?.price)}</p>
                             </div>
     
-                            <button className='text-sm bg-red-600 hover:bg-red-700 text-white px-3 py-0.5 rounded-full' onClick={(e) => addToCart(e, product?._id)}>Add to Cart</button>
+                            <button className='text-sm bg-red-600 hover:bg-red-700 text-white px-3 py-0.5 rounded-full' onClick={(e) => handleAddToCart(e, product?._id)}>Add to Cart</button>
                         
                         </div>
                     </Link>
