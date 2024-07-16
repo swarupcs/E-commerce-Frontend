4f95f29cb669f5984b777cb8ba9d82f7fac30ea3
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-07-16 17:00:53 +0530

    product zoom functionality

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import SummaryApi from '../common'
 import { FaStar } from "react-icons/fa";
@@ -21,6 +21,12 @@ const ProductDetails = () => {
   const [loading,setLoading] = useState(true)
   const productImageListLoading = new Array(4).fill(null)
   const [activeImage,setActiveImage] = useState("")
+
+  const [zoomImageCoordinate,setZoomImageCoordinate] = useState({
+    x : 0,
+    y : 0
+  })
+  const [zoomImage,setZoomImage] = useState(false)
 
   const fetchProductDetails = async()=>{
     setLoading(true)
@@ -53,6 +59,25 @@ const ProductDetails = () => {
     setActiveImage(imageURL);
   }
 
+  const handleZoomImage = useCallback((e) =>{
+    setZoomImage(true)
+    const { left , top, width , height } = e.target.getBoundingClientRect()
+    console.log("coordinate", left, top , width , height)
+
+    const x = (e.clientX - left) / width
+    const y = (e.clientY - top) / height
+
+    setZoomImageCoordinate({
+      x,
+      y
+    })
+  },[zoomImageCoordinate])
+
+  const handleLeaveImageZoom = ()=>{
+    setZoomImage(false)
+  }
+
+
   return (
     <div className='container mx-auto p-4'>
 
@@ -61,8 +86,27 @@ const ProductDetails = () => {
           {/* Product Image */}
         <div className='h-96 flex flex-col lg:flex-row-reverse gap-4'>
 
-          <div className='h-[300px] w-[300px] lg:h-96 lg:w-96 bg-slate-200'>
-            <img src={activeImage} alt=""  className='h-full w-full object-scale-down mix-blend-multiply'/>
+          <div className='h-[300px] w-[300px] lg:h-96 lg:w-96 bg-slate-200 relative'>
+            <img src={activeImage} alt=""  className='h-full w-full object-scale-down mix-blend-multiply' onMouseMove={handleZoomImage} onMouseLeave={handleLeaveImageZoom}/>
+
+            {/* product zoom */}
+            {
+                      zoomImage && (
+                        <div className='hidden lg:block absolute min-w-[500px] overflow-hidden min-h-[400px] bg-slate-200 p-1 -right-[510px] top-0'>
+                          <div
+                            className='w-full h-full min-h-[400px] min-w-[500px] mix-blend-multiply scale-150'
+                            style={{
+                              background : `url(${activeImage})`,
+                              backgroundRepeat : 'no-repeat',
+                              backgroundPosition : `${zoomImageCoordinate.x * 100}% ${zoomImageCoordinate.y * 100}% `
+    
+                            }}
+                          >
+    
+                          </div>
+                        </div>
+                      )
+                    }
 
           </div>
             <div className='h-full'>
