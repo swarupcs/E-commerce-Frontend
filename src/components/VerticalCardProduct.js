6981d9ab327d67d4c661e21213cd7ee7382daf55
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-07-16 17:07:15 +0530

    some functionality in vertical card product

--- src/components/VerticalCardProduct.js
+++ src/components/VerticalCardProduct.js
@@ -4,6 +4,7 @@ import displayINRCurrency from './../helpers/displayCurrency';
 import { FaAngleRight } from "react-icons/fa6";
 import { FaAngleLeft } from "react-icons/fa6";
 import addToCart from '../helpers/addToCart';
+import { Link } from 'react-router-dom';
 
 const VerticalCardProduct = ({category,heading}) => {
 
@@ -67,7 +68,7 @@ const VerticalCardProduct = ({category,heading}) => {
 
                 data.map((product, index) => {
                     return(
-                        <div className='w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow'>
+                        <Link  to={"product/"+product?._id} className='w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow'>
                             <div className='bg-slate-200 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center'>
                                 <img src={product.productImage[0]} alt="" className='object-scale-down h-full hover:scale-110 transition-all mix-blend-multiply'/>
                             </div>
@@ -82,7 +83,7 @@ const VerticalCardProduct = ({category,heading}) => {
                             <button className='text-sm bg-red-600 hover:bg-red-700 text-white px-3 py-0.5 rounded-full' onClick={(e) => addToCart(e, product?._id)}>Add to Cart</button>
                         
                         </div>
-                    </div>
+                    </Link>
                     )
                 })
             )
