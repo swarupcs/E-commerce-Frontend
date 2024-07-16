ef620471050be7e58808520dfb47f57a5b1d6886
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-07-16 19:13:02 +0530

    change toast position

--- src/App.js
+++ src/App.js
@@ -56,7 +56,9 @@ function App() {
       cartProductCount, // current user add to cart product count,
       fetchUserAddToCart,
     }}>
-    <ToastContainer />
+    <ToastContainer 
+      position='top-center'
+    />
       <Header/>
       <main className='min-h-[calc(100vh-120px)] pt-16'>
       <Outlet/>
